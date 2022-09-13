75bf4a8b823b44721c0577f7b61d24113a0854a1
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2022-09-13 21:38:22 +0900

    add misc files

---
+++ README.md
@@ -0,0 +1,35 @@
+EcorisMap
+===================================
+EcorisMapは、スマホで位置情報を記録、確認できるアプリです。
+
+EcorisMap is an application that records locations and information with your smartphone.
+
+
+  
+Install
+-------------
+
+https://ecoris-map.web.app
+
+
+Documentation
+-------------
+
+  [English Document](https://ecoris-map.web.app/manual_en.html)
+  
+  [日本語のドキュメント](https://ecoris-map.web.app/manual_ja.html)
+
+
+Change Log
+--------------------------------------------
+ver. 0.1.1
+- beta release
+
+
+
+License
+=======
+
+ EcorisMap is released under the XXX License. XXX is under consideration.
+
+_Copyright (c) 2022 ECORIS Inc._
--- android/app/src/main/java/jp/co/ecoris/ecorismap/MainActivity.java
+++ android/app/src/main/java/jp/co/ecoris/ecorismap/MainActivity.java
@@ -9,6 +9,19 @@ import com.facebook.react.ReactRootView;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
+import android.content.Intent;
+import android.content.ContentResolver;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
+import android.util.Log;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -54,4 +67,93 @@ public class MainActivity extends ReactActivity {
     // because it's doing more than {@link Activity#moveTaskToBack} in fact.
     super.invokeDefaultOnBackPressed();
   }
+
+  // // on your MainActivity Class body
+@Override
+public void onNewIntent(Intent intent) {
+  super.onNewIntent(intent);
+  setIntent(intent);
+  Uri data = getIntent().getData();
+        if(data != null) {
+            try {
+                Log.d("ecorismapLog", "Start Import");
+                importData(data);
+            }catch (Exception e) {
+                Log.e("File Import Error", e.getMessage());
+            }
+        }
+   getIntent().setData(null);
+}
+
+
+
+  @Override
+    protected void onResume() {
+        super.onResume();
+        Uri data = getIntent().getData();
+        if(data != null) {
+            try {
+                Log.d("ecorismapLog", "Start Import");
+                importData(data);
+            }catch (Exception e) {
+                Log.e("File Import Error", e.getMessage());
+            }
+        }
+         getIntent().setData(null);
+    }
+
+
+  private void importData(Uri data) {
+
+      final String scheme = data.getScheme();
+      if (ContentResolver.SCHEME_CONTENT.equals(scheme)) {
+          try {
+              ContentResolver cr = getApplicationContext().getContentResolver();
+              InputStream is = cr.openInputStream(data);
+              if(is == null) return;
+
+              String name = getContentName(cr, data);
+              Log.d("ecorismapLog", name);
+              PackageManager m = getPackageManager();
+              String s = getPackageName();
+              PackageInfo p = m.getPackageInfo(s, 0);
+              s = p.applicationInfo.dataDir;
+
+              InputStreamToFile(is, s + "/cache/" + name);
+          } catch (Exception e) {
+              Log.e("File Import Error", e.getMessage());
+          }
+      }
+  }
+
+  private String getContentName(ContentResolver resolver, Uri uri){
+      Cursor cursor = resolver.query(uri, null, null, null, null);
+      
+      int nameIndex = cursor.getColumnIndex(MediaStore.MediaColumns.DISPLAY_NAME);
+      cursor.moveToFirst();
+      if (nameIndex >= 0) {
+          return cursor.getString(nameIndex);
+      } else {
+          return null;
+      }
+  }
+
+  private void InputStreamToFile(InputStream in, String file) {
+      try {
+          OutputStream out = new FileOutputStream(new File(file));
+
+          int size = 0;
+          byte[] buffer = new byte[1024];
+
+          while ((size = in.read(buffer)) != -1) {
+              out.write(buffer, 0, size);
+          }
+
+          out.close();
+      }
+      catch (Exception e) {
+          Log.e("MainActivity", "InputStreamToFile exception: " + e.getMessage());
+      }
+  }
+
 }
