d8c07814ed7925347c237e00551c0d43280f78dc
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2022-12-10 11:17:03 +0900

    fix gpx check of import and export

---
+++ src/__tests__/resources/invalid_track_gpx.ts
@@ -0,0 +1,11 @@
+const gpx = `<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<gpx>
+<trk>
+<trkseg>
+<trkpt lat="142.4982489180" lon="139.8534992896"></trkpt>
+<trkpt lat="42.4982588924a" lon="139.8534111120"><ele></ele><time></time></trkpt>
+</trkseg>
+</trk>
+</gpx>`;
+
+export default gpx;
