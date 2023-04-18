4ca2bfd0bd489c03fa28f3effb43dcf7f731b49a
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2023-04-18 16:43:18 +0900

    WIP MapMemo resized version

--- src/components/organisms/HomeCanvasView.tsx
+++ src/components/organisms/HomeCanvasView.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useContext, useMemo, useRef, useState } from 'react';
 import { GestureResponderEvent, PanResponder, PanResponderInstance, View } from 'react-native';
-import { COLOR, TILE_FOLDER } from '../../constants/AppConstants';
+import { TILE_FOLDER } from '../../constants/AppConstants';
 import { useWindow } from '../../hooks/useWindow';
 import { HomeContext } from '../../contexts/Home';
 import { ExpoWebGLRenderingContext, GLView } from 'expo-gl';
@@ -9,165 +9,135 @@ import * as FileSystem from 'expo-file-system';
 import * as ImageManipulator from 'expo-image-manipulator';
 import { latToTileY, lonToTileX, tileToLatLon } from '../../utils/Tile';
 import { latLonToXY } from '../../utils/Coords';
-import ImageResizer from '@bam.tech/react-native-image-resizer';
-import * as MediaLibrary from 'expo-media-library';
-import { decode } from 'fast-png';
-import { Buffer } from 'buffer';
-import RNFS from 'react-native-fs';
+import { RegionType } from '../../types';
 
 export const CanvasView = () => {
   const { mapViewRef, setVisibleMapMemo } = useContext(HomeContext);
   const [gl, setGl] = useState<ExpoWebGLRenderingContext | null>(null);
   const [ctx, setCtx] = useState<Expo2DContext | null>(null);
+  const [tiles, setTiles] = useState<
+    { x: number; y: number; z: number; topLeftXY: number[]; bottomRightXY: number[] }[]
+  >([]);
 
-  const drawNum = useRef(0);
   const offset = useRef([0, 0]);
   const { windowHeight, windowWidth, devicePixelRatio: dpr, mapRegion } = useWindow();
+  const TILE_MAX_SIXE = 512;
+  const tilesForRegion = useCallback(
+    (region: RegionType) => {
+      const regionTiles: {
+        x: number;
+        y: number;
+        z: number;
+        topLeftXY: number[];
+        bottomRightXY: number[];
+        size: number;
+      }[] = [];
+      const { latitude, longitude, latitudeDelta, longitudeDelta, zoom } = region;
+      const topLeftLat = latitude + latitudeDelta / 2;
+      const topLeftLon = longitude - longitudeDelta / 2;
+      const bottomRightLat = latitude - latitudeDelta / 2;
+      const bottomRightLon = longitude + longitudeDelta / 2;
 
-  // eslint-disable-next-line @typescript-eslint/no-shadow
-  async function loadTileImage(ctx: Expo2DContext) {
-    const { latitude, longitude, latitudeDelta, longitudeDelta, zoom } = mapRegion;
+      const topLeftTileX = lonToTileX(topLeftLon, zoom);
+      const topLeftTileY = latToTileY(topLeftLat, zoom);
+      const bottomRightTileX = lonToTileX(bottomRightLon, zoom);
+      const bottomRightTileY = latToTileY(bottomRightLat, zoom);
 
-    const topLeftLat = latitude + latitudeDelta / 2;
-    const topLeftLon = longitude - longitudeDelta / 2;
-    const bottomRightLat = latitude - latitudeDelta / 2;
-    const bottomRightLon = longitude + longitudeDelta / 2;
+      for (let x = topLeftTileX; x <= bottomRightTileX; x++) {
+        for (let y = topLeftTileY; y <= bottomRightTileY; y++) {
+          const tileTopLeftLatLon = tileToLatLon(x, y, zoom);
+          const tileBottomRightLatLon = tileToLatLon(x + 1, y + 1, zoom);
+          const topLeftXY = latLonToXY(
+            [tileTopLeftLatLon.lon, tileTopLeftLatLon.lat],
+            mapRegion,
+            { width: windowWidth, height: windowHeight },
+            mapViewRef.current
+          );
+          const bottomRightXY = latLonToXY(
+            [tileBottomRightLatLon.lon, tileBottomRightLatLon.lat],
+            mapRegion,
+            { width: windowWidth, height: windowHeight },
+            mapViewRef.current
+          );
+          const size = bottomRightXY[0] - topLeftXY[0];
 
-    const topLeftTileX = lonToTileX(topLeftLon, zoom);
-    const topLeftTileY = latToTileY(topLeftLat, zoom);
-    const bottomRightTileX = lonToTileX(bottomRightLon, zoom);
-    const bottomRightTileY = latToTileY(bottomRightLat, zoom);
-
-    for (let x = topLeftTileX; x <= bottomRightTileX; x++) {
-      for (let y = topLeftTileY; y <= bottomRightTileY; y++) {
-        const tileTopLeftLatLon = tileToLatLon(x, y, zoom);
-        const tileBottomRightLatLon = tileToLatLon(x + 1, y + 1, zoom);
-        const topLeftXY = latLonToXY(
-          [tileTopLeftLatLon.lon, tileTopLeftLatLon.lat],
-          mapRegion,
-          { width: windowWidth, height: windowHeight },
-          mapViewRef.current
-        );
-        const bottomRightXY = latLonToXY(
-          [tileBottomRightLatLon.lon, tileBottomRightLatLon.lat],
-          mapRegion,
-          { width: windowWidth, height: windowHeight },
-          mapViewRef.current
-        );
-
-        const x0 = topLeftXY[0];
-        const y0 = topLeftXY[1];
-        const x1 = bottomRightXY[0];
-        const y1 = bottomRightXY[1];
-        const width = (x1 - x0) * dpr;
-        const height = (y1 - y0) * dpr;
-        const imagePath = `${TILE_FOLDER}/mapmemo/${zoom}/${x}/${y}`;
-
-        const info = await FileSystem.getInfoAsync(imagePath);
-        if (info.exists) {
-          //const resizedImage = await ImageResizer.createResizedImage(imagePath, width, height, 'PNG', 100);
-          //const uri = resizedImage.uri as string;
-
-          // //permissionの確認
-          // const { status } = await MediaLibrary.requestPermissionsAsync();
-          // if (status === 'granted') {
-          //   const asset = await MediaLibrary.createAssetAsync(uri);
-          //   await MediaLibrary.createAlbumAsync('DownLoads', asset);
-          //   console.log(uri);
-          // }
-          const resizedImage = await ImageManipulator.manipulateAsync(imagePath, [], {
-            compress: 1,
-            format: ImageManipulator.SaveFormat.PNG,
-          });
-          //@ts-ignore
-          resizedImage.localUri = resizedImage.uri;
-          //@ts-ignore
-          ctx.drawImage(resizedImage, (800 + x0) * dpr, (800 + y0) * dpr);
-          ctx.flush();
-          //console.log(resizedImage);
-          // const uri = imagePath;
-          // const data = await RNFS.readFile(uri, 'base64');
-          // const png = decode(Buffer.from(data, 'base64'));
-          // const imageData = { data: png.data as Uint8ClampedArray, width: width, height: height };
-          // ctx.putImageData(imageData, (800 + x0) * dpr, (800 + y0) * dpr, 0, 0, imageData.width, imageData.height);
+          regionTiles.push({ x: x, y: y, z: zoom, topLeftXY, bottomRightXY, size });
         }
       }
-    }
-  }
+
+      return regionTiles;
+    },
+    [mapRegion, mapViewRef, windowHeight, windowWidth]
+  );
+
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  const loadTileImage = async (ctx: Expo2DContext) => {
+    const regionTiles = tilesForRegion(mapRegion);
+
+    const loadImagePromise = regionTiles.map((tile) => {
+      const imagePath = `${TILE_FOLDER}/mapmemo/${tile.z}/${tile.x}/${tile.y}`;
+      FileSystem.getInfoAsync(imagePath).then((info) => {
+        if (!info.exists) return;
+        console.log(tile.size);
+        return ImageManipulator.manipulateAsync(imagePath, [], {
+          compress: 1,
+          format: ImageManipulator.SaveFormat.PNG,
+        }).then((image) => {
+          //@ts-ignore
+          image.localUri = image.uri;
+          //@ts-ignore
+          ctx.drawImage(image, (TILE_MAX_SIXE + tile.topLeftXY[0]) * dpr, (TILE_MAX_SIXE + tile.topLeftXY[1]) * dpr);
+          ctx.flush();
+        });
+      });
+    });
+
+    await Promise.all(loadImagePromise);
+    return regionTiles;
+  };
 
   const saveMapMemo = useCallback(async () => {
     //console.log(gl);
     if (!gl) return;
-    const { latitude, longitude, latitudeDelta, longitudeDelta, zoom } = mapRegion;
+    //const tiles = tilesForRegion(mapRegion);
 
-    const topLeftLat = latitude + latitudeDelta / 2;
-    const topLeftLon = longitude - longitudeDelta / 2;
-    const bottomRightLat = latitude - latitudeDelta / 2;
-    const bottomRightLon = longitude + longitudeDelta / 2;
+    const makeDirPromise = tiles.map((tile) =>
+      FileSystem.makeDirectoryAsync(`${TILE_FOLDER}/mapmemo/${tile.z}/${tile.x}`, {
+        intermediates: true,
+      })
+    );
+    await Promise.all(makeDirPromise);
 
-    const topLeftTileX = lonToTileX(topLeftLon, zoom);
-    const topLeftTileY = latToTileY(topLeftLat, zoom);
-    const bottomRightTileX = lonToTileX(bottomRightLon, zoom);
-    const bottomRightTileY = latToTileY(bottomRightLat, zoom);
-
-    for (let x = topLeftTileX; x <= bottomRightTileX; x++) {
-      for (let y = topLeftTileY; y <= bottomRightTileY; y++) {
-        const tileTopLeftLatLon = tileToLatLon(x, y, zoom);
-        const tileBottomRightLatLon = tileToLatLon(x + 1, y + 1, zoom);
-        const topLeftXY = latLonToXY(
-          [tileTopLeftLatLon.lon, tileTopLeftLatLon.lat],
-          mapRegion,
-          { width: windowWidth, height: windowHeight },
-          mapViewRef.current
-        );
-        const bottomRightXY = latLonToXY(
-          [tileBottomRightLatLon.lon, tileBottomRightLatLon.lat],
-          mapRegion,
-          { width: windowWidth, height: windowHeight },
-          mapViewRef.current
-        );
-
-        const x0 = topLeftXY[0];
-        const y0 = topLeftXY[1];
-        const x1 = bottomRightXY[0];
-        const y1 = bottomRightXY[1];
-
-        // // 4. 切り抜いたタイル画像をアプリのストレージに保存
-
-        const glSnapshot = await GLView.takeSnapshotAsync(gl, {
-          format: 'png',
-          rect: {
-            x: (800 + x0) * dpr,
-            y: (1600 + windowHeight - (800 + y0 + y1 - y0)) * dpr, //GLViewの座標系は左下が原点なので、y座標を反転させてy1側を原点にする
-            width: (x1 - x0) * dpr,
-            height: (y1 - y0) * dpr,
-          },
+    const saveImagePromise = tiles.map(async (tile) => {
+      const [x0, y0] = tile.topLeftXY;
+      const [x1, y1] = tile.bottomRightXY;
+      const width = (x1 - x0) * dpr;
+      const height = (y1 - y0) * dpr;
+      console.log('width:', width, 'height:', height);
+      const x = (TILE_MAX_SIXE + x0) * dpr;
+      const y = (TILE_MAX_SIXE + windowHeight - y1) * dpr; //GLViewの座標系は左下が原点なので、y座標を反転させてy1側を原点にする
+      return await GLView.takeSnapshotAsync(gl, {
+        format: 'png',
+        rect: {
+          x,
+          y,
+          width,
+          height,
+        },
+      }).then((glSnapshot) => {
+        ImageManipulator.manipulateAsync(glSnapshot.uri as string, [], {
+          compress: 1,
+          format: ImageManipulator.SaveFormat.PNG,
+        }).then((image) => {
+          FileSystem.moveAsync({
+            from: image.uri as string,
+            to: `${TILE_FOLDER}/mapmemo/${tile.z}/${tile.x}/${tile.y}`,
+          });
         });
-        // console.log(
-        //   'x:' + (800 + x0) * dpr + ' y:' + (1600 + windowHeight - (800 + y1)) * dpr,
-        //   'width:' + (x1 - x0) * dpr,
-        //   'height:' + (y1 - y0) * dpr,
-        //   'y0:',
-        //   y0
-        // );
-        const uri = glSnapshot.uri as string;
-        // 256x256のサイズにリサイズ
-        //console.log(glSnapshot.width);
-        //const resizedImage = await ImageResizer.createResizedImage(glSnapshot.uri as string, 512, 512, 'PNG', 100, 0);
-        //const uri = resizedImage.uri as string;
-
-        const folder = `${TILE_FOLDER}/mapmemo/${zoom}/${x}`;
-        await FileSystem.makeDirectoryAsync(folder, {
-          intermediates: true,
-        });
-        const destinationUri = `${folder}/${y}`;
-        await FileSystem.moveAsync({
-          from: uri,
-          to: destinationUri,
-        });
-      }
-    }
-  }, [dpr, gl, mapRegion, mapViewRef, windowHeight, windowWidth]);
+      });
+    });
+    await Promise.all(saveImagePromise);
+  }, [dpr, gl, tiles, windowHeight]);
 
   const panResponder: PanResponderInstance = useMemo(
     () =>
@@ -184,7 +154,6 @@ export const CanvasView = () => {
           ];
 
           ctx.beginPath();
-          drawNum.current = 0;
         },
         onPanResponderMove: (event: GestureResponderEvent) => {
           if (!ctx) return;
@@ -192,24 +161,20 @@ export const CanvasView = () => {
             (event.nativeEvent.pageX + offset.current[0]) * dpr,
             (event.nativeEvent.pageY + offset.current[1]) * dpr
           );
-          // if (drawNum.current % 10 === 0) {
           ctx.stroke();
           ctx.flush();
-          // }
-          drawNum.current++;
         },
         onPanResponderRelease: async () => {
           if (!ctx) return;
           ctx.stroke();
           ctx.flush();
-          gl!.endFrameEXP();
+
           await saveMapMemo();
           setVisibleMapMemo(false);
           setVisibleMapMemo(true);
-          console.log('saveMapMemo');
         },
       }),
-    [ctx, dpr, gl, saveMapMemo, setVisibleMapMemo]
+    [ctx, dpr, saveMapMemo, setVisibleMapMemo]
   );
 
   const options: Expo2dContextOptions = {
@@ -225,8 +190,9 @@ export const CanvasView = () => {
     const ctx = new Expo2DContext(gl, options);
     ctx.lineWidth = 5;
     ctx.strokeStyle = 'red';
-    await loadTileImage(ctx);
-    console.log('loadTileImage');
+    const regionTiles = await loadTileImage(ctx);
+    //console.log(regionTiles);
+    setTiles(regionTiles);
     setCtx(ctx);
     setGl(gl);
   };
@@ -244,9 +210,9 @@ export const CanvasView = () => {
     >
       <GLView
         style={{
-          width: windowWidth + 1600,
-          height: windowHeight + 1600,
-          transform: [{ translateX: -800 }, { translateY: -800 }],
+          width: windowWidth + TILE_MAX_SIXE * 2,
+          height: windowHeight + TILE_MAX_SIXE * 2,
+          transform: [{ translateX: -TILE_MAX_SIXE }, { translateY: -TILE_MAX_SIXE }],
         }}
         onContextCreate={onContextCreate}
         {...panResponder.panHandlers}
