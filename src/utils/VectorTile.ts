941dea20de3cea7725517d9b0a08fa54e2216b9d
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2023-10-07 19:17:41 +0900

    fix info

--- src/utils/VectorTile.ts
+++ src/utils/VectorTile.ts
@@ -16,54 +16,50 @@ export const fetchVectorTileInfo = async (
 ) => {
   //const LAYER_NAME = '北上川H30';
   //console.log(tile.z, tile.x, tile.y);
+  try {
+    const maxDistanceToLine = getMetersPerPixelAtZoomLevel(latlon[1], tile.z) * 10;
 
-  const maxDistanceToLine = getMetersPerPixelAtZoomLevel(latlon[1], tile.z) * 10;
+    const localLocation = `${TILE_FOLDER}/${tileMapId}/${tile.z}/${tile.x}/${tile.y}.pbf`;
+    const info = await FileSystem.getInfoAsync(localLocation);
+    //console.log(info);
+    if (!info.exists) return undefined;
+    const base64String = await FileSystem.readAsStringAsync(localLocation);
 
-  const localLocation = `${TILE_FOLDER}/${tileMapId}/${tile.z}/${tile.x}/${tile.y}.pbf`;
-  if (!(await FileSystem.getInfoAsync(localLocation)).exists) {
-    //console.log('not found');
-    return;
-  }
-  const base64String = await FileSystem.readAsStringAsync(localLocation);
+    const binaryData = Buffer.from(base64String, 'base64');
 
-  const binaryData = Buffer.from(base64String, 'base64');
+    const pbf = new Pbf(binaryData);
+    const layers = new VectorTile.VectorTile(pbf).layers;
 
-  const pbf = new Pbf(binaryData);
-  const layers = new VectorTile.VectorTile(pbf).layers;
-
-  for (const layerName of Object.keys(layers)) {
-    const layer = layers[layerName];
-    for (let i = 0; i < layer.length; i++) {
-      const feature = layer.feature(i);
-      const geometry = feature.toGeoJSON(tile.x, tile.y, tile.z);
-      try {
+    for (const layerName of Object.keys(layers)) {
+      const layer = layers[layerName];
+      //console.log(layerName, tile, layer.length);
+      for (let i = 0; i < layer.length; i++) {
+        const feature = layer.feature(i);
+        const geometry = feature.toGeoJSON(tile.x, tile.y, tile.z);
+        //console.log(i, geometry.geometry.type);
         if (geometry.geometry.type === 'Point') {
-          if (distance(point(latlon), point(geometry.geometry.coordinates), { units: 'meters' }) <= maxDistanceToLine) {
+          if (distance(point(latlon), point(geometry.geometry.coordinates), { units: 'meters' }) <= maxDistanceToLine)
             return feature.properties;
-          }
         } else if (geometry.geometry.type === 'LineString') {
           if (
             pointToLineDistance(point(latlon), lineString(geometry.geometry.coordinates), { units: 'meters' }) <=
             maxDistanceToLine
-          ) {
+          )
             return feature.properties;
-          }
         } else if (geometry.geometry.type === 'Polygon') {
-          //@ts-ignore
-          const coords = geometry.geometry.coordinates[0];
-          if (coords.length > 3) {
-            const isInside = booleanPointInPolygon(point(latlon), polygon([coords]));
-            if (isInside) {
-              return feature.properties;
-              //console.log(feature.properties);
-            } else {
-              //console.log('not found');
-            }
+          const coords = geometry.geometry.coordinates;
+          const isInside = booleanPointInPolygon(point(latlon), polygon(coords));
+          if (isInside) return feature.properties;
+        } else if (geometry.geometry.type === 'MultiPolygon') {
+          for (const coords of geometry.geometry.coordinates) {
+            const isInside = booleanPointInPolygon(point(latlon), polygon(coords));
+            if (isInside) return feature.properties;
           }
         }
-      } catch (e) {
-        console.log(e);
       }
     }
+  } catch (e) {
+    console.log(e);
+    return undefined;
   }
 };
