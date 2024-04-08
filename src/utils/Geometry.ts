780e7f21661df132c05fa0ad70ad97e158b3bb05
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-04-08 17:47:48 +0900

    change export mapmemo stamp as point

--- src/utils/Geometry.ts
+++ src/utils/Geometry.ts
@@ -318,6 +318,10 @@ export const generateCSV = (
       break;
     case 'LINE':
       geometries = dataSet.map(({ coords }) => {
+        if ((coords as LocationType[]).length === 1) {
+          //MapMemoのSTAMPの場合
+          return `"POINT(${(coords as LocationType[])[0].longitude} ${(coords as LocationType[])[0].latitude})"`;
+        }
         const linestring = (coords as LocationType[]).map((coord) => `${coord.longitude} ${coord.latitude}`).join(',');
         return `"LINESTRING(${linestring})"`;
       });
@@ -471,6 +475,25 @@ export const generateGeoJson = (
             }
           : {};
 
+        if ((record.coords as LocationType[]).length === 1) {
+          //MapMemoのSTAMPの場合
+          const coordinates = [
+            (record.coords as LocationType[])[0].longitude,
+            (record.coords as LocationType[])[0].latitude,
+          ];
+          const feature = {
+            type: 'Feature',
+            properties: {
+              ...properties,
+              ...mapMemoProperties,
+            },
+            geometry: {
+              type: 'Point',
+              coordinates: coordinates,
+            },
+          };
+          return feature;
+        }
         const coordinates = (record.coords as LocationType[]).map((coords) => [coords.longitude, coords.latitude]);
         const feature = {
           type: 'Feature',
