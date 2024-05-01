50ec1735f65479c9c77deb38402500b283590436
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-05-01 18:22:12 +0900

    Fix account button marginBottom for web platform

--- src/components/organisms/HomeAccountButton.tsx
+++ src/components/organisms/HomeAccountButton.tsx
@@ -41,7 +41,7 @@ export const HomeAccountButton = () => {
       color: COLOR.WHITE,
       fontSize: 20,
       fontWeight: 'bold',
-      marginBottom: 5,
+      marginBottom: Platform.OS === 'web' ? 0 : 5,
     },
   });
   return (
