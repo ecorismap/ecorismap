832160c02d6fabdf3b4ce93ebdc190ed2774ac01
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-05-02 17:29:54 +0900

    add multi line string to DatEdit

--- src/components/organisms/DataEditString.tsx
+++ src/components/organisms/DataEditString.tsx
@@ -7,13 +7,41 @@ import { COLOR } from '../../constants/AppConstants';
 interface Props {
   name: string;
   value: string | number | undefined;
+  multiline?: boolean;
   editable?: boolean;
   onChangeText: (name: string, value: string) => void;
   onEndEditing: () => void;
 }
 
 export const DataEditString = (props: Props) => {
-  const { name, value, editable, onChangeText, onEndEditing } = props;
+  const { name, value, multiline, editable, onChangeText, onEndEditing } = props;
+
+  const styles = StyleSheet.create({
+    input: {
+      backgroundColor: COLOR.GRAY0,
+      borderRadius: 5,
+      flex: 2,
+      fontSize: 16,
+      height: 40,
+      paddingHorizontal: 12,
+      paddingLeft: 10,
+    },
+
+    td: {
+      alignItems: 'center',
+      borderBottomWidth: 1,
+      borderColor: COLOR.GRAY2,
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      paddingHorizontal: 10,
+      paddingVertical: 0,
+    },
+    tr: {
+      flexDirection: 'row',
+      height: multiline ? 140 : 70,
+    },
+  });
 
   return (
     <View style={styles.tr}>
@@ -26,35 +54,9 @@ export const DataEditString = (props: Props) => {
           onEndEditing={onEndEditing}
           onBlur={onEndEditing}
           editable={editable}
+          multiline={multiline}
         />
       </View>
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  input: {
-    backgroundColor: COLOR.GRAY0,
-    borderRadius: 5,
-    flex: 2,
-    fontSize: 16,
-    height: 40,
-    paddingHorizontal: 12,
-    paddingLeft: 10,
-  },
-
-  td: {
-    alignItems: 'center',
-    borderBottomWidth: 1,
-    borderColor: COLOR.GRAY2,
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    paddingHorizontal: 10,
-    paddingVertical: 0,
-  },
-  tr: {
-    flexDirection: 'row',
-    height: 70,
-  },
-});
