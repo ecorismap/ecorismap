35c69d71e4d4030e18030c03add7e46fc718acfd
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-05-23 19:16:46 +0900

    refactor: Update Button component props and add tooltip functionality

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -3,8 +3,38 @@ import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { COLOR } from '../../constants/AppConstants';
 
-const Button = React.memo((props: any) => {
-  const { onPress, name, size = 20, borderRadius, color, backgroundColor, style, tooltipText } = props;
+interface Props {
+  id?: string;
+  disabled?: boolean;
+  onPress?: () => void;
+  onLongPress?: () => void;
+  onPressCustom?: () => void;
+  name: any;
+  size?: number;
+  borderRadius?: number;
+  color?: string;
+  backgroundColor?: string;
+  borderColor?: string;
+  borderWidth?: number;
+  style?: any;
+  tooltipText?: string;
+}
+
+const Button = React.memo((props: Props) => {
+  const {
+    disabled,
+    onPress,
+    onLongPress,
+    name,
+    size = 20,
+    borderRadius,
+    color,
+    backgroundColor,
+    borderColor,
+    borderWidth,
+    style,
+    tooltipText,
+  } = props;
   const [showTooltip, setShowTooltip] = useState(false);
 
   const handlePressIn = () => {
@@ -19,7 +49,9 @@ const Button = React.memo((props: any) => {
     button: {
       alignItems: 'center',
       backgroundColor: backgroundColor || COLOR.BLUE,
+      borderColor: borderColor || COLOR.BLACK,
       borderRadius: borderRadius || 50,
+      borderWidth: borderWidth || 0,
       height: 35,
       justifyContent: 'center',
       width: 35,
@@ -45,7 +77,14 @@ const Button = React.memo((props: any) => {
           <Text style={styles.tooltipText}>{tooltipText || 'Tooltip'}</Text>
         </View>
       )}
-      <Pressable onPress={onPress} onHoverIn={handlePressIn} onHoverOut={handlePressOut} style={[styles.button, style]}>
+      <Pressable
+        disabled={disabled}
+        onPress={onPress}
+        onLongPress={onLongPress}
+        onHoverIn={handlePressIn}
+        onHoverOut={handlePressOut}
+        style={[styles.button, style]}
+      >
         <MaterialCommunityIcons name={name} size={size} color={color || COLOR.WHITE} />
       </Pressable>
     </View>
