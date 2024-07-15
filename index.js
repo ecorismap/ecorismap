e01ae998393b67a048a7f218e9828b09b0637c87
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-07-15 10:08:32 +0900

    fix for web

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 import 'react-native-gesture-handler';
 import 'react-native-get-random-values';
 import { registerRootComponent } from 'expo';
-
+import { createRoot } from 'react-dom/client';
+import { createElement } from 'react';
+import { Platform } from 'react-native';
 import App from './src/App';
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(App);
+if (Platform.OS === 'web') {
+  const rootTag = createRoot(document.getElementById('root') ?? document.getElementById('main'));
+  rootTag.render(createElement(App));
+} else {
+  registerRootComponent(App);
+}
