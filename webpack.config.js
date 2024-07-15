42f8fc289b9a1ab7f4077bb75f93fb40b18bc7ab
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-07-15 11:33:33 +0900

    update reanimated and babel and webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,20 @@ module.exports = async function (env, argv) {
     },
     argv
   );
+  // https://github.com/webpack/webpack/issues/11467#issuecomment-691873586
   config.module.rules.push({
     test: /.m?js/,
     resolve: {
       fullySpecified: false,
     },
+  });
+  // https://github.com/expo/expo-webpack-integrations/issues/10
+  //  from "react-native-reanimated": "^3.14.0",
+  config.module.rules.forEach((rule) => {
+    if (rule.oneOf instanceof Array) {
+      rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
+    }
+    return rule;
   });
   return config;
 };
