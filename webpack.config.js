14b302c923a28cdcc021b7968cbc5beab627d0e8
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-07-15 10:08:32 +0900

    fix for web

--- webpack.config.js
+++ webpack.config.js
@@ -14,5 +14,11 @@ module.exports = async function (env, argv) {
       fullySpecified: false,
     },
   });
+  config.module.rules.forEach((rule) => {
+    if (rule.oneOf instanceof Array) {
+      rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|jsx|cjs|ts|tsx)$/, /\.html$/, /\.json$/];
+    }
+    return rule;
+  });
   return config;
 };
