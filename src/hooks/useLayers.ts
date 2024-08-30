ec4fe50bb5af91f2d9a8ad80a3494991c0bfe91a
Takayuki Mizutani <mizutani.takayuki@gmail.com>
2024-08-30 12:03:23 +0900

    change to export kml with color

--- src/hooks/useLayers.ts
+++ src/hooks/useLayers.ts
@@ -1,10 +1,8 @@
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { LayerType, RecordType } from '../types';
+import { LayerType } from '../types';
 import { RootState } from '../store';
 import { cloneDeep } from 'lodash';
-import { t } from '../i18n/config';
-import dayjs from '../i18n/dayjs';
 
 import { updateLayerAction, setLayersAction } from '../modules/layers';
 
@@ -162,25 +160,3 @@ export const useLayers = (): UseLayersReturnType => {
     changeLayerOrder,
   } as const;
 };
-
-export function generateLabel(layer: LayerType, feature: RecordType) {
-  return layer.label === t('common.custom')
-    ? layer.customLabel
-        ?.split('|')
-        .map((f) => {
-          const fieldName = f.trim(); // Remove leading and trailing whitespaces
-          if (fieldName.startsWith('"') || fieldName.startsWith("'")) {
-            return fieldName.substring(1, fieldName.length - 1); // Remove quotes
-          } else {
-            return feature.field[fieldName];
-          }
-        })
-        .join('') || '' // Remove space between joined items
-    : layer.label === ''
-    ? ''
-    : feature.field[layer.label]
-    ? layer.field.find((f) => f.name === layer.label)?.format === 'DATETIME'
-      ? dayjs(feature.field[layer.label].toString()).format('L HH:mm')
-      : feature.field[layer.label].toString()
-    : '';
-}
