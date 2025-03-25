2e7134b6d608a70448576522657eccfabd95279a
tmizu23 <mizutani.takayuki@gmail.com>
2025-03-25 12:28:50 +0900

    babel.config.jsを修正し、webプラットフォーム用のプラグインを条件付きで追加。iOSのデプロイメントターゲットを18.0に更新し、package.jsonのwebスクリプトを修正。

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,18 @@
-module.exports = {
-  presets: ['babel-preset-expo'],
-  plugins: ['@babel/plugin-proposal-export-namespace-from',
-    ['babel-plugin-transform-define', { __DEV__: __DEV__ }],
-    'react-native-reanimated/plugin'],
+module.exports = function (api) {
+  api.cache(true);
+
+  const plugins = [
+    '@babel/plugin-proposal-export-namespace-from',
+  ];
+
+  if (process.env.PLATFORM === 'web') {
+    plugins.push(['babel-plugin-transform-define', { __DEV__: __DEV__ }]);
+  }
+
+  plugins.push('react-native-reanimated/plugin');
+
+  return {
+    presets: ['babel-preset-expo'],
+    plugins,
+  };
 };
