91762812068592b95fbf6e806a281b46f753e8c8
tmizu23 <mizutani.takayuki@gmail.com>
2025-03-25 10:24:58 +0900

    babel.config.jsにbabel-plugin-transform-defineを追加し、package.jsonとyarn.lockを更新

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   presets: ['babel-preset-expo'],
   plugins: ['@babel/plugin-proposal-export-namespace-from',
+    ['babel-plugin-transform-define', { __DEV__: __DEV__ }],
     'react-native-reanimated/plugin'],
 };
