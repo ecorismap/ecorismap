c55b6d68eab9cb0b70fdc05757231019b5bb5104
tmizu23 <mizutani.takayuki@gmail.com>
2025-04-16 12:57:49 +0900

    Gdalwarp.hを修正し、New Architectureに対応

--- modules/react-native-gdalwarp/ios/Gdalwarp.h
+++ modules/react-native-gdalwarp/ios/Gdalwarp.h
@@ -1,12 +1,5 @@
 
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNGdalwarpSpec.h"
 
-@interface Gdalwarp : NSObject <NativeGdalwarpSpec>
-#else
 #import <React/RCTBridgeModule.h>
-
 @interface Gdalwarp : NSObject <RCTBridgeModule>
-#endif
-
 @end
